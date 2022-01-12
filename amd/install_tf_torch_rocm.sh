a5c6fd55be34a214f8a0ac4074af76e5f5873c23
Kyle Gerard Felker <felker@anl.gov>
2022-01-12 00:20:56 +0000

    First shared install finished with modulefile

--- amd/install_tf_torch_rocm.sh
+++ amd/install_tf_torch_rocm.sh
@@ -5,8 +5,14 @@
 # SUSE RPMs for zypper (2021-10-19)
 # https://repo.radeon.com/rocm/zyp/zypper/
 
+# Run this script in /soft/conda/amd/
+# Modulefile in /soft/modulefiles/conda/amd/
+INSTALL_DIR="${PWD}/2022-01-11/"
 
-cd $HOME
+set -e
+
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
 mkdir miopen-install
 mkdir hipfft-install
 mkdir miopen-deps
@@ -16,51 +22,62 @@ git clone https://github.com/ROCmSoftwarePlatform/MIOpen.git
 git clone https://github.com/ROCmSoftwarePlatform/hipFFT.git
 git clone https://github.com/ROCmSoftwarePlatform/rccl.git
 
+
+. /etc/profile.d/modules.sh  # get module shell command
 module use /soft/modulefiles; module load cmake
 
-cd $HOME/rccl
+cd ${INSTALL_DIR}/rccl
 mkdir build; cd build
-CXX=/usr/bin/hipcc cmake -DCMAKE_INSTALL_PREFIX=$HOME/rccl-install ..
+CXX=/usr/bin/hipcc cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rccl-install ..
 make -j 8
 make install
-export LD_LIBRARY_PATH=$HOME/rccl-install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${INSTALL_DIR}/rccl-install/lib:$LD_LIBRARY_PATH
 
 
-cd $HOME/hipFFT
+cd ${INSTALL_DIR}/hipFFT
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/hipfft-install -DCMAKE_CXX_COMPILER=hipcc -DCMAKE_BUILD_TYPE=Release -DBUILD_CLIENTS=OFF -L ..
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/hipfft-install -DCMAKE_CXX_COMPILER=hipcc -DCMAKE_BUILD_TYPE=Release -DBUILD_CLIENTS=OFF -L ..
 make -j 8
 make install
-export LD_LIBRARY_PATH=$HOME/hipfft-install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${INSTALL_DIR}/hipfft-install/lib:$LD_LIBRARY_PATH
 
-cd $HOME/MIOpen
-cmake -P install_deps.cmake --prefix $HOME/miopen-deps
+cd ${INSTALL_DIR}/MIOpen
+cmake -P install_deps.cmake --prefix ${INSTALL_DIR}/miopen-deps
 
 # KGF: needed?
-#export LD_LIBRARY_PATH=$HOME/miopen-deps:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=${INSTALL_DIR}/miopen-deps:$LD_LIBRARY_PATH
 
 
 mkdir build; cd build
-CXX=/opt/rocm/llvm/bin/clang++ cmake -DMIOPEN_BACKEND=HIP -DCMAKE_INSTALL_PREFIX=$HOME/miopen-install -DCMAKE_PREFIX_PATH=$HOME/miopen-deps ..
+CXX=/opt/rocm/llvm/bin/clang++ cmake -DMIOPEN_BACKEND=HIP -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/miopen-install -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/miopen-deps ..
 make -j 8
 make install
-export LD_LIBRARY_PATH=$HOME/miopen-install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${INSTALL_DIR}/miopen-install/lib:$LD_LIBRARY_PATH
 
-cd $HOME
-export MYPATH=/home/$USER/amdenv
-conda create -p $MYPATH python=3.8 pip
+cd ${INSTALL_DIR}
+export MYPATH=${INSTALL_DIR}/mconda3
+conda create -y -p $MYPATH python=3.8 pip
 # KGF: check that the above line pulls python 3.8.12 or so, not 3.8.0
 conda activate $MYPATH
 pip install tensorflow-rocm
 
-
-# testing; possible bug in TF 2.6.0 / Keras with "multiple optimizers"
-git clone https://github.com/tensorflow/benchmarks.git tf_benchmarks || true
-python $HOME/tf_benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=resnet50
-
-
 # https://pytorch.org/get-started/locally/
 pip install torch torchvision==0.11.1 -f https://download.pytorch.org/whl/rocm4.2/torch_stable.html
 
+echo "Cleaning up"
 
-#export LD_LIBRARY_PATH=$HOME/rccl-install/lib:$HOME/hipfft-install/lib:$HOME/miopen-deps:$HOME/miopen-install/lib:$LD_LIBRARY_PATH
+conda list
+
+chmod -R a-w ${INSTALL_DIR}
+
+set +e
+
+#----------------------------------------------------
+
+# testing; possible bug in TF 2.6.0 / Keras with "multiple optimizers"
+#git clone https://github.com/tensorflow/benchmarks.git tf_benchmarks || true
+#python $HOME/tf_benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=resnet50
+
+
+# user (or modulefile) should modify their library path as follows:
+#export LD_LIBRARY_PATH=${INSTALL_DIR}/rccl-install/lib:${INSTALL_DIR}/hipfft-install/lib:${INSTALL_DIR}/miopen-deps:${INSTALL_DIR}/miopen-install/lib:$LD_LIBRARY_PATH
